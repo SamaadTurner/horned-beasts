ddb76065c5fa679edf68634a14328ec8747762fd
SamaadTurner <samaad.turner@gmail.com>
2023-08-15 14:55:37 -0700

    finished feature 2

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,51 +1,37 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import Image1 from './images/HornedBeast_1.jpeg';
-import Image2 from './images/HornedBeast_2.jpeg';
-import Image3 from './images/HornedBeast_3.jpeg';
-
+import hornedBeastPics from '../src/assets/data.json';
 
 class Gallery extends React.Component {
-    render() {
-        let data = [
-          {
-            num: 1,
-            image_url:
-              Image1,
-            title: 'RPG HornedBeast',
-            description: 'Got this picture from google',
-            keyword: 'hornedBeast_1',
-            horns: 1,
-          },
-    
-          {
-            num: 2,
-            image_url:
-            Image2,
-          title: 'Horsed Beast',
-          description: 'white beast',
-          keyword: 'hornedBeast_2',
-          horns: 2,
-          },
-    
-          {
-            num: 3,
-            image_url:
-              Image3,
-            title: 'Fantasy Horned Beast',
-            description: 'cool mythical beast',
-            keyword: 'hornedBeast_3',
-            horns: 6,
-          },
-        ];
-        return (
-          <div>
-            {data.map((item, num) => 
-              <HornedBeast key={num} title={item.title} image_url={item.image_url} keyword={item.keyword} description={item.description} />
-            )}
-          </div>
-        );
-      }
-    }
-    
+  render() {
+    return (
+      <div>
+        {hornedBeastPics.map((item) => (
+          <HornedBeast
+            key={item._id}
+            title={item.title}
+            image_url={item.image_url}
+            keyword={item.keyword}
+            description={item.description}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
 export default Gallery;
+
+
+
+
+
+
+
+
+
+// task 1: Create state inside of the HornedBeast component that keeps track 
+//of the number of times each beast has been favorited.
+
+//task 2: Put a heart in each HornedBeast component 
+//with the number of “Favorites” next to it.
--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,18 +1,51 @@
 import React from 'react';
+import blackHeart from './images/heart-icon.png';
+import emptyHeart from './images/heart-line-icon.png';
 
 class HornedBeast extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isFavorite: false,
+      favoriteCounter: 0, // added state to track favorite count
+    };
   }
+
+  handleClick = () => {
+    this.setState((prevState) => ({
+      isFavorite: !prevState.isFavorite,
+      favoriteCounter: prevState.isFavorite ? prevState.favoriteCounter - 1 : prevState.favoriteCounter + 1,
+    }));
+  };
+
   render() {
     return (
       <div>
         <h2>{this.props.title}</h2>
         <img src={this.props.image_url} alt={this.props.keyword} title={this.props.title}></img>
         <p>{this.props.description}</p>
+        <button onClick={this.handleClick}>
+          <img src={this.state.isFavorite ? blackHeart : emptyHeart} alt={this.state.isFavorite ? "Black Heart" : "Empty Heart"} />
+          {this.state.favoriteCounter} Favorites
+        </button>
       </div>
     );
   }
 }
-
 export default HornedBeast;
+
+
+
+// task 1: Create state inside of the HornedBeast component that keeps track 
+//of the number of times each beast has been favorited.
+
+//task 2: Put a heart in each HornedBeast component 
+//with the number of “Favorites” next to it.
+
+
+// If isFavorite is true:
+// blackHeart(icon) <favoriteCount> Favorites
+
+
+//If isFavorite is false:
+// emptyHeart(icon) <favoriteCount> Favorites
