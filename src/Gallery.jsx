226556907bc3c20aeb943f0c8171f7845703a86d
SamaadTurner <samaad.turner@gmail.com>
2023-08-17 22:59:57 -0700

    finished lab04

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,14 +3,19 @@ import HornedBeast from './HornedBeast';
 import SelectedBeasts from './SelectedBeasts';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+
 
 
 class Gallery extends React.Component {
   constructor() {
     super();
+    // state: the status of the component
+    // this. means literally the component you are in
     this.state = {
       showModal: false,
       selectedBeast: null,
+      hornFilterCount: '',
     };
   }
 
@@ -28,13 +33,49 @@ class Gallery extends React.Component {
     });
   };
 
+
+  filterHornedBeasts = (beasts) => {
+    if(this.state.hornFilterCount === ''){
+      return beasts;
+    }
+    return beasts.filter((beast) => beast.horns.toString() === this.state.hornFilterCount);
+
+  };
+
+  changeFilter = (e) => {
+    this.setState({
+      hornFilterCount: e.target.value,
+      
+    })
+    console.log(this.state);
+  }
+
   render() {
     const { hornedBeasts } = this.props; // Make sure to pass this prop
+    const filteredBeasts = this.filterHornedBeasts(hornedBeasts);
+    console.log(filteredBeasts);
+
 
     return (
       <div className='gallery'>
+        <Form> 
+          <Form.Group as= {Row} className='mb-3'
+          controlId='hornFiler'>
+            <Form.Label>
+              Filter By Horn Count
+            </Form.Label>
+            <Form.Control as="select" value={this.state.hornFilterCount} onChange = {this.changeFilter}>
+            <option value=''>None</option>
+              <option value='1'>One</option>
+              <option value='2'>Two</option>
+              <option value='3'>Three</option>
+              
+              
+            </Form.Control>
+          </Form.Group>
+        </Form>
         <Row xs={1} md={3} lg={4}>
-        {hornedBeasts.map((item) => (
+        {filteredBeasts.map((item) => (
           <Col  key={item._id}>
           <HornedBeast
             item={item}
